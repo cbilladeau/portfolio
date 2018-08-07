d04af5ec1ed6c869aa9e144cd6f3931243dceaf2
Cour Billadeau <cbilladeau@billet-deaux.com>
2018-08-06 21:04:58 -0500

    1.1.0 asset reorg & package update

--- src/assets/components/CodeBarChart.js
+++ src/assets/components/CodeBarChart.js
@@ -1,34 +1,33 @@
-import {Bar} from 'vue-chartjs'
+import {HorizontalBar} from 'vue-chartjs'
 
 export default {
-    extends: Bar,
+    extends: HorizontalBar,
     mounted () {
-        this.gradient = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 300);
-        this.gradient.addColorStop(1, 'rgba(68, 49, 141, 0.9)');
-        this.gradient.addColorStop(0, 'rgba(216, 63 , 135, 0.80)');
+        this.gradient = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 150, 0);
+        this.gradient.addColorStop(0, 'rgba(68, 49, 141, 0.9)');
+        this.gradient.addColorStop(1, 'rgba(216, 63 , 135, 0.80)');
 
-        this.gradientHover = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 300);
-        this.gradientHover.addColorStop(1, 'rgba(68, 49, 141, 1)');
-        this.gradientHover.addColorStop(0, 'rgba(216, 63 , 135, 1)');
+        this.gradientHover = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 150, 0);
+        this.gradientHover.addColorStop(0, 'rgba(68, 49, 141, 1)');
+        this.gradientHover.addColorStop(1, 'rgba(216, 63 , 135, 1)');
 
         this.addPlugin({
             id: 'chartjs-plugin-datalabels'
         })
         this.renderChart({
-            labels: ['Photoshop', 'Illustrator', 'InDesign', 'UXPin', 'Axure'],
+            labels: ['HTML5', 'CSS', 'SASS, LESS, Stylus', 'JS', 'jQuery', 'VueJS', 'PHP'],
             datasets: [{
                 backgroundColor: this.gradient,
                 hoverBackgroundColor: this.gradientHover,
-                data: [100, 70, 60, 80, 70],
+                data: [100, 95, 80, 65, 70, 50, 50],
                 datalabels: {
                     font: {
                         family: 'Geo Sans, sans-serif',
                         size: 16
                     },
                     color: 'rgba(255,255,255,1)',
-                    align: 'top',
+                    align: 'right',
                     anchor: 'start',
-                    rotation: -90,
                     formatter: function(value, context) {
                         return context.chart.data.labels[context.dataIndex];
                     }
@@ -36,32 +35,23 @@ export default {
             }, {
                 backgroundColor: 'rgba(0,0,0,0.12)',
                 hoverBackgroundColor: 'rgba(0,0,0,0.12)',
-                data: [0, 30, 40, 20, 30],
+                data: [0, 5, 20, 35, 30, 50, 50],
                 datalabels: {
                     display: false,
-                    // display: true,
-                    // align: 'bottom',
-                    // anchor: 'end',
-                    // color: 'rgba(0,0,0,0.56)',
-                    // font: {
-                    //     family: 'Typewriter Condensed, sans-serif',
-                    //     size: 14
-                    // },
-                    // formatter: function(value, context) {
-                    //     return (100-value) + '%';
-                    // }
                 }
-            // }, {
-            //     backgroundColor: 'rgba(0,0,0,0.12)',
-            //     hoverBackgroundColor: 'rgba(0,0,0,0.12)',
-            //     data: [100, 100, 100, 100, 100, 100, 100],
-            //     datalabels: {
-            //         display: false,
-            //     }
+            }, {
+                backgroundColor: 'rgba(0,0,0,0.12)',
+                hoverBackgroundColor: 'rgba(0,0,0,0.12)',
+                data: [100, 100, 100, 100, 100, 100, 100],
+                datalabels: {
+                    display: false,
+                }
             }],
         }, {
             scales: {
                 yAxes: [{
+                    barPercentage: 0.7,
+                    categoryPercentage: 1,
                     stacked: true,
                     ticks: {
                         display: false
@@ -72,8 +62,6 @@ export default {
                     }
                 }],
                 xAxes: [{
-                    barPercentage: 0.6,
-                    categoryPercentage: 1,
                     stacked: true,
                     ticks: {
                         display: false,
